59cdc5b2f1ec70a9e0fb35d8f6386dc3fb3a0ea8
Sakib Sami <s4kibs4mi@gmail.com>
2018-04-27 01:18:41 +0600

    Update

---
+++ README.md
@@ -0,0 +1,3 @@
+# Yo-no-superhero
+
+Tutorial : [You no superhero, queue your tasks](https://medium.com/@sakibsami/you-no-superhero-queue-your-tasks-b9d821e8fa8a)
--- tasks/notify_subscribers.go
+++ tasks/notify_subscribers.go
@@ -4,13 +4,13 @@ import (
 	"fmt"
 	"strings"
 	"time"
+	"github.com/RichardKnop/machinery/v1/tasks"
 )
 
 func NotifySubscriber(args ...string) error {
 	fmt.Println(fmt.Sprintf("Notifying to : %s", args[0]))
 	if strings.HasPrefix(args[0], "a") || strings.HasPrefix(args[0], "c") {
-		return tasks.NewErrRetryTaskLater("some error", 4 * time.Hour)
-		return fmt.Errorf("forced error to requeue task")
+		return tasks.NewErrRetryTaskLater("forced error to requeue task", 10*time.Second)
 	}
 	return nil
 }
